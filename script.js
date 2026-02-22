6cb4ed6fba504d7a1bc18311e7348bf89d7f7ae8
TGF Nins <fngsabulao0921@gmail.com>
2026-02-22 22:01:34 +0800

    added js code FOR LOOP

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 alert("Hello World");
 console.log("Hello World");
+
+// FOR loop \\
+
+//for(variable; condition; operation)
+
+//number sequence
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
+
+//string sequence
+for(let i = 0;i < 5; i++){
+    console.log("Hello World");
+}
