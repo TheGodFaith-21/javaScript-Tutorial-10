02fa8c99bc98a0ca38af724d707ee31141ae498a
TGF Nins <fngsabulao0921@gmail.com>
2026-02-22 22:19:20 +0800

    Added js code reverse iterate array

--- script.js
+++ script.js
@@ -20,7 +20,11 @@ for(let i = 0;i < 5; i++){
 // ITERATE ARRAY \\
 
 let names = ["Kan","Ken","Kin","Kon","Kun"];
-
+//forward
 for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+//reverse
+for(let i = names.length-1; i >= 0; i--){
     console.log(names[i]);
 }
